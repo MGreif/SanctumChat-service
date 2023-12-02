edd7bde06a88bce1e2997a4157b71d6466b26c23
“Mika <mika.greif@googlemail.com>
2023-12-02 01:02:02 +0100

    Cleanup

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,5 @@ COPY . .
 RUN apt clean && apt update && apt install -y pkg-config libssl-dev gcc-multilib libpq-dev libudev-dev
 RUN cargo install diesel_cli --no-default-features --features postgres
 RUN cargo build --release
+
 CMD ["./target/release/rust-websocket-server"]
--- src/main.rs
+++ src/main.rs
@@ -62,7 +62,7 @@ async fn main() {
         .with_state(app_state)
         .with_state(config.clone());
     
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
